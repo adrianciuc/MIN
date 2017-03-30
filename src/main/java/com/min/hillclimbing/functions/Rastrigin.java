c912141e660ff0508ebb7d7882488641eeb79e0a
Adrian Ciucanel <adrian.ciucanel@outlook.com>
2017-03-30 18:11:29 +0300

    Add Rastrigin function

---
+++ src/main/java/com/min/hillclimbing/functions/Rastrigin.java
@@ -0,0 +1,16 @@
+package com.min.hillclimbing.functions;
+
+class Rastrigin extends Function {
+
+    Rastrigin(double lowerBound,
+              double upperBound,
+              int numberOfElementsInSolution,
+              int sizeOfElementInSolution) {
+        super(lowerBound, upperBound, numberOfElementsInSolution, sizeOfElementInSolution);
+    }
+
+    @Override
+    public Double evaluateFor(double[][] input) {
+        return null;
+    }
+}
