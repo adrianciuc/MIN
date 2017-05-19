e77f686c64ffb84fb4c3873517fe543483a96e72
Adrian Ciucanel <adrian.ciucanel@outlook.com>
2017-05-19 17:30:48 +0300

    Add minimum documentation for resolving MinMaxTSP problem

---
+++ src/main/java/com/min/tsp/Solution.java
@@ -0,0 +1,13 @@
+package com.min.tsp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    private final List<List<Integer>> representation;
+
+    public Solution() {
+        representation = new ArrayList<>();
+    }
+}
